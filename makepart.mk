256a8adb2dd00d623c97eb704591ddad86bd2ef2
Tetsuo Honda <t-honda@hondarer-soft.com>
2025-12-31 17:18:32 +0900

    サブモジュール更新

--- makepart.mk
+++ makepart.mk
@@ -81,3 +81,13 @@ INCDIR += \
     $(WORKSPACE_FOLDER)/testfw/include
 
 LIBSDIR     =
+
+# ライブラリの出力先をプラットフォームごとに設定
+# Set library output directory based on platform
+ifneq ($(OS),Windows_NT)
+    # Linux: TARGET_ARCH (e.g., linux-el8-x64)
+    OUTPUT_DIR := $(WORKSPACE_FOLDER)/testfw/lib/$(TARGET_ARCH)
+else
+    # Windows: TARGET_ARCH/MSVC_CRT_SUBDIR (e.g., windows-x64/md)
+    OUTPUT_DIR := $(WORKSPACE_FOLDER)/testfw/lib/$(TARGET_ARCH)/$(MSVC_CRT_SUBDIR)
+endif
