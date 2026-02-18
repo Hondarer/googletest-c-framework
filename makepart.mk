b7f1796fd370a7a41faa90907e8c31c76df333cc
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-02-18 22:30:29 +0900

    調整

--- makepart.mk
+++ makepart.mk
@@ -1,19 +1,20 @@
+GCC_WARN_BASE = \
+	-Wall -Wextra \
+	-Wformat=2 \
+	-Wshadow -Wundef \
+	-Wpointer-arith -Wcast-qual -Wcast-align \
+	-Wswitch-enum -Wswitch-default \
+	-Wpacked -Wpadded \
+	-Wunknown-pragmas
+GCC_WARN_C_ONLY = \
+	-Wmissing-prototypes \
+	-Wstrict-prototypes \
+	-Wmissing-declarations
+
 ifneq ($(OS),Windows_NT)
     # Linux
-    WARN_BASE = \
-		-Wall -Wextra \
-		-Wformat=2 \
-		-Wshadow -Wundef \
-		-Wpointer-arith -Wcast-qual -Wcast-align \
-		-Wswitch-enum -Wswitch-default \
-		-Wpacked -Wpadded \
-		-Wunknown-pragmas
-    WARN_C_ONLY = \
-		-Wmissing-prototypes \
-		-Wstrict-prototypes \
-		-Wmissing-declarations
-    CFLAGS     += $(WARN_BASE) $(WARN_C_ONLY)
-    CXXFLAGS   += $(WARN_BASE)
+    CFLAGS     += $(GCC_WARN_BASE) $(GCC_WARN_C_ONLY)
+    CXXFLAGS   += $(GCC_WARN_BASE)
     LDFLAGS     =
 else
     # Windows
