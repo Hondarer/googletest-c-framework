3e6ab3453cd9b232d9ef8844d4c6f39a0eaa6397
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-02-18 22:27:47 +0900

    警告オプションの調整

--- makepart.mk
+++ makepart.mk
@@ -1,73 +1,19 @@
 ifneq ($(OS),Windows_NT)
     # Linux
-    CFLAGS=\
-		-Wall \
-		-Wextra \
-		-Wcast-align \
-		-Wdisabled-optimization \
-		-Wfloat-equal \
+    WARN_BASE = \
+		-Wall -Wextra \
 		-Wformat=2 \
-		-Winit-self \
-		-Winline \
-		-Winvalid-pch \
-		-Wmissing-format-attribute \
-		-Wmissing-noreturn \
-		-Wpacked \
-		-Wpadded \
-		-Wredundant-decls \
-		-Wstrict-aliasing=2 \
-		-Wswitch-default \
-		-Wswitch-enum \
-		-Wundef \
-		-Wvariadic-macros \
-		-Wshadow \
-		-Wmissing-declarations \
+		-Wshadow -Wundef \
+		-Wpointer-arith -Wcast-qual -Wcast-align \
+		-Wswitch-enum -Wswitch-default \
+		-Wpacked -Wpadded \
+		-Wunknown-pragmas
+    WARN_C_ONLY = \
 		-Wmissing-prototypes \
 		-Wstrict-prototypes \
-		-Wsequence-point \
-		-Wpointer-arith \
-		-Wdeclaration-after-statement \
-		-Wbad-function-cast \
-		-Wunsafe-loop-optimizations \
-		-Wnested-externs \
-		-Wcast-qual \
-		-Wint-to-pointer-cast \
-		-Wpointer-to-int-cast \
-		-Wimplicit-fallthrough=0 \
-		-Wformat-overflow=0 \
-		-Wunknown-pragmas \
-		-W
-    CXXFLAGS=\
-		-Wall \
-		-Wextra \
-		-Wcast-align \
-		-Wdisabled-optimization \
-		-Wfloat-equal \
-		-Wformat=2 \
-		-Winit-self \
-		-Winline \
-		-Winvalid-pch \
-		-Wmissing-format-attribute \
-		-Wmissing-noreturn \
-		-Wpacked \
-		-Wpadded \
-		-Wredundant-decls \
-		-Wstrict-aliasing=2 \
-		-Wswitch-default \
-		-Wswitch-enum \
-		-Wundef \
-		-Wvariadic-macros \
-		-Wshadow \
-		-Wmissing-declarations \
-		-Wsequence-point \
-		-Wpointer-arith \
-		-Wunsafe-loop-optimizations \
-		-Wcast-qual \
-		-Wint-to-pointer-cast \
-		-Wimplicit-fallthrough=0 \
-		-Wformat-overflow=0 \
-		-Wunknown-pragmas \
-		-W
+		-Wmissing-declarations
+    CFLAGS     += $(WARN_BASE) $(WARN_C_ONLY)
+    CXXFLAGS   += $(WARN_BASE)
     LDFLAGS     =
 else
     # Windows
