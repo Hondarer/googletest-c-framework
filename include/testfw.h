769cfd6e37f634b5e74465c68c3e3b08f45e2f3e
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-05-04 13:00:40 +0900

    弱参照実装のマルチプラットフォーム対応

--- include/testfw.h
+++ include/testfw.h
@@ -4,12 +4,12 @@
 #ifndef _WIN32
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpadded"
-#endif // _WIN32
+#endif /* _WIN32 */
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #ifndef _WIN32
 #pragma GCC diagnostic pop
-#endif // _WIN32
+#endif /* _WIN32 */
 
 #include <format_attr.h>
 #include <test_com.h>
@@ -22,8 +22,13 @@ using namespace testing;
 
 #ifndef _WIN32
 #define WEAK_ATR __attribute__((weak))
-#else // _WIN32
+#define MOCK_WEAK_IMPL(rettype, func, ...) \
+    __attribute__((weak)) rettype func(__VA_ARGS__)
+#else /* _WIN32 */
 #define WEAK_ATR
-#endif // _WIN32
+#define MOCK_WEAK_IMPL(rettype, func, ...) \
+    __pragma(comment(linker, "/ALTERNATENAME:" #func "=_mock_impl_" #func)) \
+    extern "C" rettype _mock_impl_##func(__VA_ARGS__)
+#endif /* _WIN32 */
 
 #endif // _TESTFW_H
