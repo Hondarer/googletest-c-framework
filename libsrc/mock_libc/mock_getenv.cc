c32d5efba6c593d1a6d717e6f40a4e8afc5ba372
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-05-12 06:37:11 +0900

    警告対処

--- libsrc/mock_libc/mock_getenv.cc
+++ libsrc/mock_libc/mock_getenv.cc
@@ -1,6 +1,8 @@
 #include <test_com.h>
 #include <mock_stdlib.h>
 
+#include <string>
+
 using namespace testing;
 
 char *delegate_real_getenv(const char *file, const int line, const char *func, const char *name)
@@ -10,7 +12,22 @@ char *delegate_real_getenv(const char *file, const int line, const char *func, c
     (void)line;
     (void)func;
 
+#ifndef _WIN32
     return getenv(name);
+#else
+    static thread_local std::string env_value;
+    char *raw_value = nullptr;
+    size_t val_len = 0;
+
+    if (_dupenv_s(&raw_value, &val_len, name) != 0 || raw_value == nullptr)
+    {
+        return nullptr;
+    }
+
+    env_value.assign(raw_value, val_len > 0 ? val_len - 1 : 0);
+    free(raw_value);
+    return const_cast<char *>(env_value.c_str());
+#endif
 }
 
 char *mock_getenv(const char *file, const int line, const char *func, const char *name)
