140de15a0be8b2df0fdc48f46a1b59b67079faf0
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-08-11 15:43:18 +0900

    OpenSSL override の再入読み込みを防止

--- include_override/openssl/rand.h
+++ include_override/openssl/rand.h
@@ -1,12 +1,21 @@
 #ifndef _OVERRIDE_OPENSSL_RAND_H
 #define _OVERRIDE_OPENSSL_RAND_H
 
-/* 本物を include */
-#include_next <openssl/rand.h>
+/*
+ * OpenSSL の実ヘッダーが相互に別のヘッダーを include する場合は、
+ * 実ヘッダーの読み込みが完了するまでモックの注入を遅延する。
+ */
+#if defined(_IN_OVERRIDE_OPENSSL_REAL_HEADERS)
+    #include_next <openssl/rand.h>
+#else
+    #define _IN_OVERRIDE_OPENSSL_REAL_HEADERS
+    #include_next <openssl/rand.h>
+    #undef _IN_OVERRIDE_OPENSSL_REAL_HEADERS
 
-/* モックにすげ替え */
-#define _IN_OVERRIDE_HEADER_OPENSSL_H
-#include <mock_openssl.h>
-#undef _IN_OVERRIDE_HEADER_OPENSSL_H
+    /* モックにすげ替え */
+    #define _IN_OVERRIDE_HEADER_OPENSSL_H
+    #include <mock_openssl.h>
+    #undef _IN_OVERRIDE_HEADER_OPENSSL_H
+#endif
 
 #endif // _OVERRIDE_OPENSSL_RAND_H
