f9ddcaa8f4fdcc44929a2c3c06a59a2faaeff879
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-08-11 19:51:43 +0900

    Windows CI エラー対策

--- include_override/fcntl.h
+++ include_override/fcntl.h
@@ -1,12 +1,21 @@
 #ifndef _OVERRIDE_FCNTL_H
 #define _OVERRIDE_FCNTL_H
 
-#ifndef _WIN32
+#ifdef _WIN32
+
+    /* 本物を include */
+    #pragma push_macro("_INC_FCNTL")
+    #undef _INC_FCNTL
+    #include <../ucrt/fcntl.h>
+    #pragma pop_macro("_INC_FCNTL")
+
+#else
     #include_next <fcntl.h>
 
     #define _IN_OVERRIDE_HEADER_FCNTL_H
     #include <mock_fcntl.h>
     #undef _IN_OVERRIDE_HEADER_FCNTL_H
+
 #endif // _WIN32
 
 #endif // _OVERRIDE_FCNTL_H
--- include_override/signal.h
+++ include_override/signal.h
@@ -1,12 +1,21 @@
 #ifndef _OVERRIDE_SIGNAL_H
 #define _OVERRIDE_SIGNAL_H
 
-#ifndef _WIN32
+#ifdef _WIN32
+
+    /* 本物を include */
+    #pragma push_macro("_INC_SIGNAL")
+    #undef _INC_SIGNAL
+    #include <../ucrt/signal.h>
+    #pragma pop_macro("_INC_SIGNAL")
+
+#else
     #include_next <signal.h>
 
     #define _IN_OVERRIDE_HEADER_SIGNAL_H
     #include <mock_signal.h>
     #undef _IN_OVERRIDE_HEADER_SIGNAL_H
+
 #endif // _WIN32
 
 #endif // _OVERRIDE_SIGNAL_H
